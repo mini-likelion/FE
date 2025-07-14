14ca2aa602f4ad4edcaeef88a9981a0b7f113431
dessert99 <lsjh1234@naver.com>
2025-07-14 11:52:39 +0900

    메인 구조

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,5 +1,14 @@
+import styles from './mainPage.module.css';
+
 const MainPage = () => {
-  return <section></section>;
+  return (
+    <div className={styles.main__wrapper}>
+      <section className={styles.main__items}>
+        <h1>반복되는 포스터</h1>
+      </section>
+      <div className={styles.main__pages}></div>
+    </div>
+  );
 };
 
 export default MainPage;
