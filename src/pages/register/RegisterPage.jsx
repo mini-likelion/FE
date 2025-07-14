edebb5a93b1c1b7f7d79ad491c1051db4a7b10db
dessert99 <lsjh1234@naver.com>
2025-07-14 16:22:33 +0900

    feat: 로그인/회원가입 페이지 및 스타일 수정

---
+++ src/pages/register/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import styles from './registerPage.module.css';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>회원가입 페이지</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
